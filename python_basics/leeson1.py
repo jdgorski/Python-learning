86e00739683938727be3e1384cb45f24dfd586b7
Gorski <jdgorski@cps.k12.il.us>
2024-09-24 14:30:31 -0500

    first commit

---
+++ python_basics/leeson1.py
@@ -0,0 +1,25 @@
+# # mkdir means make directory
+# cd means change directory
+# ls means list directory
+# pwd means print working directory
+# touch means create a file
+# # rm means remove a file
+# #  pound sign means comment
+# print("Hello, World!")
+# print("I love Mr. Evins")
+# print("age \t 25!")
+# name = input("What is your name? ")
+# print("Hello " + name, "i'm Computer" )
+# # # \t means tab
+# age = input("What is your age? ")
+# print(age)
+name = input("what is your name? ")
+print("Hello," + name)
+
+age = input("How old are you? ")
+print( "you are," + age, "years old ")
+address = input("What is your address? ")
+print(address)
+school = input("What school do you go to? ")
+print(school)
+print (name + "is, " + age, "years old, lives, " + address, "and goes to " + school,)
